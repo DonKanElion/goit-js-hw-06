c963328a8d292fd6dbd61b3362ec83bd17237722
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-09-13 23:05:19 +0300

    solution task 1, 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,43 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listWithId = document.querySelector('ul#ingredients');
+console.log(listWithId);
+
+// // Old school
+
+// const ingredientsArray = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//     const ingredient = ingredients[i];
+
+//     const ingredEl = document.createElement('li');
+//     ingredEl.classList.add('item');
+//     ingredEl.textContent = ingredient;
+//     console.log(ingredEl);
+
+//     ingredientsArray.push(ingredEl);
+// }
+
+// console.log(ingredientsArray);
+// console.log(...ingredientsArray);
+
+// listWithId.append(...ingredientsArray);
+
+//  New school через  Map():
+
+const element = ingredients.map(ingredient => {
+  const ingredEl = document.createElement('li');
+    ingredEl.classList.add('item');
+    ingredEl.textContent = ingredient;
+    console.log(ingredEl);
+    return ingredEl;
+});
+
+console.log(element);
+listWithId.append(...element);
+
+
+
